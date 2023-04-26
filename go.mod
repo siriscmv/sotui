26f00f4ec14364fa494faab832f0eb587c2a8da6
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-04-26 11:45:00 +0530

    non-working impl

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/Siris01/sotui
 go 1.20
 
 require (
+	github.com/charmbracelet/bubbles v0.15.0
+	github.com/charmbracelet/bubbletea v0.23.2
 	github.com/charmbracelet/glamour v0.6.0
+	github.com/charmbracelet/lipgloss v0.7.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/rocketlaunchr/google-search v1.1.5
 )
@@ -15,8 +18,10 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly/v2 v2.1.0 // indirect
@@ -27,8 +32,11 @@ require (
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
@@ -39,7 +47,9 @@ require (
 	github.com/yuin/goldmark v1.5.2 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- tui.go
+++ tui.go
@@ -1,1 +1,178 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var tui *tea.Program
+
+type (
+	errMsg  error
+	State   int
+	LogType int
+)
+
+const (
+	WaitingForInput State = iota
+	WaitingForResponse
+	DisplayingAllQuestions
+	DisplayingQuestion
+	DisplayingAllComments
+	DisplayingHelpScreen
+)
+
+const (
+	Info LogType = iota
+	Warning
+	Error
+)
+
+type model struct {
+	table    table.Model
+	textarea textarea.Model
+	viewport viewport.Model
+	spinner  spinner.Model
+	mouse    bool
+	response SEResponse
+	state    State
+	err      error
+}
+
+var (
+	BoldStyle    = lipgloss.NewStyle().Bold(true)
+	InfoLogStyle = BoldStyle.Copy().
+			Foreground(lipgloss.Color("#a6da95"))
+	WarningLogStyle = BoldStyle.Copy().
+			Foreground(lipgloss.Color("#eed49f"))
+	ErrorLogStyle = BoldStyle.Copy().
+			Foreground(lipgloss.Color("#ed8796"))
+	UserStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#c6a0f6"))
+	FadedStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("#999999"))
+	HelpStyle      = FadedStyle.Copy().Italic(true).Padding(0, 1).Margin(0, 1)
+	ContainerStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("#c6a0f6")).Padding(1).Margin(1)
+)
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "What is your question?"
+	ta.Focus()
+
+	ta.Prompt = UserStyle.Render("❯ ")
+	ta.CharLimit = 2000
+
+	ta.SetWidth(30)
+	ta.SetHeight(1)
+
+	ta.FocusedStyle.CursorLine = ta.FocusedStyle.CursorLine.Copy().UnsetBackground()
+	ta.ShowLineNumbers = false
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	vp := viewport.New(30, 3)
+	vp.MouseWheelEnabled = true
+
+	sp := spinner.New()
+	sp.Spinner = spinner.Dot
+	sp.Style = UserStyle
+
+	table := table.New()
+
+	return model{
+		table:    table,
+		textarea: ta,
+		viewport: vp,
+		spinner:  sp,
+		response: SEResponse{},
+		state:    WaitingForInput,
+		err:      nil,
+		mouse:    true,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+		spCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	m.spinner, spCmd = m.spinner.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Height = msg.Height - 10
+		m.viewport.Width = msg.Width - 6
+		m.textarea.SetWidth(msg.Width - 6)
+
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlS:
+			if m.mouse {
+				m.mouse = false
+				return m, tea.Sequence(tea.DisableMouse, getLogCmd("Disabled mouse scroll/clicks", Info))
+			} else {
+				m.mouse = true
+				return m, tea.Sequence(tea.EnableMouseCellMotion, getLogCmd("Enabled mouse scroll/clicks", Info))
+			}
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+
+			return m, tea.Batch(tiCmd, vpCmd, spinner.Tick)
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd, spCmd)
+}
+
+func (m model) View() string {
+	var bottomView string
+
+	view := ContainerStyle.Render(
+		fmt.Sprintf(
+			"%s\n\n%s",
+			m.viewport.View(),
+			lipgloss.NewStyle().Width(m.viewport.Width).Render(bottomView),
+		),
+	)
+
+	return view
+}
+
+func RunTUI() {
+	var m = initialModel()
+	tui = tea.NewProgram(m, tea.WithMouseCellMotion())
+
+	if _, err := tui.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func getLogCmd(msg string, logType LogType) tea.Cmd {
+	return func() tea.Msg {
+		switch logType {
+		case Info:
+		case Warning:
+		case Error:
+		}
+
+		return nil
+	}
+}
